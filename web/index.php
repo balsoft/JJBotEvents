fa8d4a8d022eae9abe1d2dd969fb405a5ba5f57e
balsoft <balsoft@yandex.ru>
2016-07-22 11:11:20 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,11 @@
 <?php
-$link = mysqli_connect('localhost', 'my_user', 'my_password', 'my_db');
+define('BOT_TOKEN', '264455520:AAE0uvEd-Ic5Qo25vgpdGY9NydnTYqAvSnI');
+  define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+  define('SQL_URL', 'MYSQL5012.Smarterasp.net');
+  define('DB_NAME','db_a0a1cf_jj');
+  define('LOGIN','a0a1cf_jj');
+  define('PASS','1q2w3e4r');
+$link = mysqli_connect(SQL_URL, LOGIN, PASS, DB_NAME);
 
 if (!$link) {
     die('Ошибка подключения (' . mysqli_connect_errno() . ') '
