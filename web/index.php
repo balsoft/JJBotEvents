945ff6e5da38b3baba4e194b7d94d3cdce8b8329
balsoft <balsoft@yandex.ru>
2016-07-22 11:09:15 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html>
-  <head><title>Загрузка таблицы</title></head>
-  <body>
-    Hello, Рита!.
-    <br>
-     <form enctype="multipart/form-data" method="post" action="/web/upload.php">
-   <p><input type="file" name="f">
-   <input type="submit" value="Отправить"></p>
-  </form> 
-  </body>
-</html>
+<?php
+$link = mysqli_connect('localhost', 'my_user', 'my_password', 'my_db');
+
+if (!$link) {
+    die('Ошибка подключения (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+}
+
+echo 'Соединение установлено... ' . mysqli_get_host_info($link) . "\n";
+
+mysqli_close($link);
+?>
