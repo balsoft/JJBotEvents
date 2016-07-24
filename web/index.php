9b821d582a7ce25aa4320a905d448c11dffaa8aa
Boris <boris2000n@gmail.com>
2016-07-24 23:05:10 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -8,18 +8,14 @@ define('BOT_TOKEN', '264455520:AAE0uvEd-Ic5Qo25vgpdGY9NydnTYqAvSnI');
 $mysqli = new mysqli(SQL_URL, LOGIN, PASS, DB_NAME);
 
 
-echo 'Соединение установлено... ' . "\n";
-
-echo 'Создаем таблицу'."\n";
 $mysqli->query("CREATE TABLE Users (name TEXT, chatID TEXT)");
-echo 'Таблица создана'."\n";
+
 
 $result = $mysqli->query("SELECT * FROM Users WHERE 1 LIMIT 0,25");
  while ($row = $result->fetch_assoc()) {
         printf ("%s (%s)\n", $row["name"], $row["chatID"]);
     }
   
-echo 'Тест';
 /* Удалить таблицу */
 
 $mysqli->close();
