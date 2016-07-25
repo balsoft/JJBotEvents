8d90fde64555801ad080e2d2266b2b94e7175807
Boris <boris2000n@gmail.com>
2016-07-25 22:16:55 +0300

    Update index2.php

--- web/index2.php
+++ web/index2.php
@@ -2,7 +2,7 @@
  function getTimeLeft($time){
     return DateTime::createFromFormat('G:i',$time, new DateTimeZone('Europe/Moscow'))->getTimestamp()-microtime(true);
   }
-  $event='22:15';
+  $event='22:24';
     if(((getTimeLeft($event)<300)&&(getTimeLeft($event)>240))||((getTimeLeft($event)<90)&&(getTimeLeft($event)>30))){
         echo 'hello';
               }
