8cc92a180f8d6ff15aa97aa737512ac6ca33cb95
Boris <boris2000n@gmail.com>
2016-07-25 13:15:48 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@ define('BOT_TOKEN', '264455520:AAE0uvEd-Ic5Qo25vgpdGY9NydnTYqAvSnI');
   define('FILE','http://my-files.ru/Save/bz1b5t/Book1.csv');
 $mysqli = new mysqli(SQL_URL, LOGIN, PASS, DB_NAME);
 /*echo DateTime::createFromFormat('G:i',$schedule[0])->sub(new DateInterval('PT5M'))->format('Y-m-d H-i-s');*/
-echo date('Y-m-d');
+echo microtime();
 /*  $scheduleRes=$mysqli->query('SELECT * FROM files WHERE 1');
   $fName=$scheduleRes->fetch_assoc()["filename"];*/
   $fName=FILE;
@@ -18,7 +18,7 @@ $schedule=fgetcsv($file,1000,';');
 $file=fopen($fName,'r');
    while (($schedule=fgetcsv($file,1000,';')) !== FALSE) {
         $num = count($data);
-            if(DateTime::createFromFormat('G:i',$schedule[0])->sub(new DateInterval('PT5M'))->getTimestamp()<time()){
+            if(DateTime::createFromFormat('G:i',$schedule[0])->sub(new DateInterval('PT5M'))->getTimestamp()<microtime()){
               {
                   while ($row = $users->fetch_assoc()){
                         $reply = $reply . ' Хорошего дня, ' . $row["name"]."Через 5 минут будет ". $schedule[1]. ". Место встречи: ".$schedule[2];
