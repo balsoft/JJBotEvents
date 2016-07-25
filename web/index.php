88c352714c78778a98771174ca84690b4f9557f9
Boris <boris2000n@gmail.com>
2016-07-25 18:14:48 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,16 +7,17 @@ define('BOT_TOKEN', '264455520:AAE0uvEd-Ic5Qo25vgpdGY9NydnTYqAvSnI');
   define('PASS','1q2w3e4r');
   define('FILE','http://my-files.ru/Save/bz1b5t/Book1.csv');
 $mysqli = new mysqli(SQL_URL, LOGIN, PASS, DB_NAME);
-echo (int)DateTime::createFromFormat('G:i','17:05')->sub(new DateInterval('PT5M'))->getTimestamp();
+
   $scheduleRes=$mysqli->query('SELECT * FROM files WHERE 1');
   $fName=$scheduleRes->fetch_assoc()["filename"];
+  echo $fName;
 /* Удалить таблицу */
 $users=$mysqli->query("SELECT * FROM Users WHERE 1 LIMIT 0,25");
 $file=fopen($fName,'r');
 $schedule=fgetcsv($file,1000,';');
    while (($schedule=fgetcsv($file,1000,';')) !== FALSE) {
         $num = count($schedule);
-            if((DateTime::createFromFormat('G:i',$schedule[0])->sub(new DateInterval('PT5M'))->getTimestamp()<microtime(true))&&(DateTime::createFromFormat('G:i',$schedule[0])->sub(new DateInterval('PT4M'))->getTimestamp()>microtime(true))){
+            if((DateTime::createFromFormat('G:i',$schedule[0], new DateTimeZone('Europe/Moscow'))->sub(new DateInterval('PT5M'))->getTimestamp()<microtime(true))&&(DateTime::createFromFormat('G:i',$schedule[0], new DateTimeZone('Europe/Moscow'))->sub(new DateInterval('PT4M'))->getTimestamp()>microtime(true))){
               {
                   while ($row = $users->fetch_assoc()){
                         $reply = $reply . ' Хорошего дня, ' . $row["name"]."Через 5 минут будет ". $schedule[1]. ". Место встречи: ".$schedule[2];
