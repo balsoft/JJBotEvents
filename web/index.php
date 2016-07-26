c99fb31cf098055487f2750591de18ef74c7da27
balsoft <balsoft@yandex.ru>
2016-07-26 20:38:23 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,9 +6,13 @@ define('BOT_TOKEN', '264455520:AAE0uvEd-Ic5Qo25vgpdGY9NydnTYqAvSnI');
   define('LOGIN','a0a1cf_jj');
   define('PASS','1q2w3e4r');
   define('FILE','http://my-files.ru/Save/bz1b5t/Book1.csv');
-  function getTimeLeft($time){
-    return DateTime::createFromFormat('G:i',$time, new DateTimeZone('Europe/Moscow'))->getTimestamp()-microtime(true);
+  function translateToEpoch($time){
+    return DateTime::createFromFormat('G:i',$time, new DateTimeZone('Europe/Moscow'))->getTimestamp();
   }
+  function getTimeLeft($time){
+    return translateToEpoch($time)-microtime(true);
+  }
+
 $mysqli = new mysqli(SQL_URL, LOGIN, PASS, DB_NAME);
 
   $scheduleRes=$mysqli->query('SELECT * FROM files WHERE 1');
@@ -18,12 +22,12 @@ $file=fopen($fName,'r');
 $schedule=fgetcsv($file,1000,';');
    while (($schedule=fgetcsv($file,1000,';')) !== FALSE) {
         $num = count($schedule);
-        echo ((string)abs(getTimeLeft($schedule[0]))).' сек до '.$schedule[1]."</br>";
-            if(abs(getTimeLeft($schedule[0]))<60.0){
+        echo ((string)abs(getTimeLeft($schedule[0])-300)).' сек до '.$schedule[1]."</br>";
+            if(abs(getTimeLeft($schedule[0])-300)<40.0){
               {
               echo 'notify';
                   while ($row = $users->fetch_assoc()){
-                        $reply = ' Хорошего дня, ' . $row["name"].". Через 1 минут будет ". $schedule[1]. ". Место встречи: ".$schedule[2];
+                        $reply = ' Хорошего дня, ' . $row["name"].". Через 1 минуту будет ". $schedule[1]. ". Место встречи: ".$schedule[2];
                          $sendto = API_URL . "sendmessage?chat_id=" . $row["chatID"] . "&text=" . $reply;
                          file_get_contents($sendto);
                   } 
