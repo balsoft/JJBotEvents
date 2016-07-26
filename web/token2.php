9a793ccbc122ce75859b4fb8d739700178dd799a
Boris <boris2000n@gmail.com>
2016-07-26 20:16:46 +0300

    Update token2.php

--- web/token2.php
+++ web/token2.php
@@ -1,26 +1,21 @@
 <?php
   define('BOT_TOKEN', '218027951:AAGFdVV7HssGRVn3WNA53BzPnozybS1NAaM');
   define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
-
-  // read incoming info and grab the chatID
   $content = file_get_contents("php://input");
   $update = json_decode($content, true);
-  $chatID = $update["message"]["chat"]["id"];
-
+  $chatID = $update["message"]["chat"]["id"]; 
   $messageText = $update["message"]["text"];
+  //sql
+  define('SQL_URL', '	MYSQL5012.Smarterasp.net');
+  define('DB_NAME','db_a0a4c0_borbd');
+  define('LOGIN','db_a0a4c0_borbd');
+  define('PASS','0013boris');
+  $mysqli = new mysqli(SQL_URL, LOGIN, PASS, DB_NAME);
+  
+  $res = $mysqli->query("SELECT * FROM 	sqlupload");
 
-  //if ($messageText == '/now') {
-  //    $reply = 'Идёт занятие по ботам.';
- // }
-
-  //if ($messageText == '/next') {
-  //    $reply = 'Спорт.';
-  //}
-
- // $reply = $reply . $messa
-
-  $reply = ' Хорошего дня, ' . $update['message']['from']['first_name'] . $messageText;
-  $reply= $content;
+  //$reply = ' Хорошего дня, ' . $update['message']['from']['first_name'] . $messageText;
+  $reply = res
 
   $sendto = API_URL . "sendmessage?chat_id=" . $chatID . "&text=" . $reply;
   file_get_contents($sendto);
