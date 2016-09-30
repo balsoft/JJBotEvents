b76ed82a7deb970a56468e1b47ed2e0140260b80
balsoft <balsoft@yandex.ru>
2016-09-30 21:27:58 +0300

    foo

--- web/index.php
+++ web/index.php
@@ -1,11 +1,12 @@
 <?php
-define('BOT_TOKEN', '264455520:AAE0uvEd-Ic5Qo25vgpdGY9NydnTYqAvSnI');
+/*define('BOT_TOKEN', '264455520:AAE0uvEd-Ic5Qo25vgpdGY9NydnTYqAvSnI');
   define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
   define('SQL_URL', 'MYSQL5012.Smarterasp.net');
   define('DB_NAME','db_a0a1cf_jj');
   define('LOGIN','a0a1cf_jj');
   define('PASS','1q2w3e4r');
-  define('FILE','http://my-files.ru/Save/bz1b5t/Book1.csv');
+  define('FILE','http://my-files.ru/Save/bz1b5t/Book1.csv');*/
+  require secret.php;
   ini_set('auto_detect_line_endings',TRUE);
   function translateToEpoch($time){
     return DateTime::createFromFormat('G:i',$time, new DateTimeZone('Europe/Moscow'))->getTimestamp();
