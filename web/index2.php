535f5b2d9f1cbea907411fa338b33ab2dba306eb
balsoft <balsoft@yandex.ru>
2016-09-30 20:18:40 +0300

    cleanup

--- web/index2.php
+++
@@ -1,9 +0,0 @@
-<?php
- function getTimeLeft($time){
-    return DateTime::createFromFormat('G:i',$time, new DateTimeZone('Europe/Moscow'))->getTimestamp()-microtime(true);
-  }
-  $event='22:24';
-    if(((getTimeLeft($event)<300)&&(getTimeLeft($event)>240))||((getTimeLeft($event)<90)&&(getTimeLeft($event)>30))){
-        echo 'hello';
-              }
-?>
